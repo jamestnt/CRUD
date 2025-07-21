5f5acd8da2b0e1cd40fb9d4e2f135b9f8d91a15c
jamestnt <michaeltnt8@gmail.com>
2025-07-20 23:12:32 -0600

    first commit

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rljxpjsylbtjelbxpbfb.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsanhwanN5bGJ0amVsYnhwYmZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwNDI4ODMsImV4cCI6MjA2ODYxODg4M30.agz-pXW9FDo7rFKPn_RfbrlB2bcIuPF5TcF0jxs3Ssw";
+const supabaseUrl = "TU_SUPABASE_URL";
+const supabaseKey = "TU_SUPABASE_ANON_KEY";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
